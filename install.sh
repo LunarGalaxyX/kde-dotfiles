be089bee85cd055487475ce18fb4f366971d8ec1
LunarGalaxy <johnnypetrie17@gmail.com>
2026-01-07 11:43:28 -0800

    Clean up install.sh by removing unused comments
    Removed commented-out sections for Fastfetch, Plasma configs, and SDDM theme.

--- install.sh
+++ install.sh
@@ -33,6 +33,12 @@ sudo pacman -Syu plasma
 #mkdir -p "$HOME/.config/kitty"
 #ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/KittyConfig.conf" "$HOME/.config/kitty/kitty.conf"
 
+# Fastfetch and configs
+
+#sudo pacman -Syu fastfetch
+#mkdir -p "$HOME/.config/fastfetch"
+#ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/FastFetchConfig.jsonc" "$HOME/.config/fastfetch/config.jsonc"
+
 # Zsh configs and theme
 
 #sudo pacman -Syu zsh
@@ -43,35 +49,5 @@ sudo pacman -Syu plasma
 #mkdir -p "$HOME/.config/zsh"
 #ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/ZshellConfig" "$HOME/.config/zsh/.zshrc"
 #ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/P10kThemeConfig.zsh" "$HOME/.config/zsh/.p10k.zsh"
-
-# Fastfetch and configs
-
-#sudo pacman -Syu fastfetch
-#mkdir -p "$HOME/.config/fastfetch"
-#ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/FastFetchConfig.jsonc" "$HOME/.config/fastfetch/config.jsonc"
-
-# Plasma configs and themes.
-
-#plasma-apply-wallpaperimage "$HOME/punpun-rice-dotfiles/dotfiles/Wallpapers/punpunWallpaper.png"
-#lookandfeeltool -a org.kde.breezedark.desktop
-#cd "$HOME/punpun-rice-dotfiles/dotfiles/KDE-Themes"
-#mv -n MateriaDark.colors "$HOME/.local/share/color-schemes/MateriaDark.colors"
-#plasma-apply-colorscheme MateriaDark
-#ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/KDE-THEMES/Materia/" "$HOME/.local/share/plasma/desktoptheme/Materia"
-#plasma-apply-desktoptheme Materia
-#ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/KDE-THEMES/YAMIS/" "$HOME/.local/share/icons/YAMIS/"
-#kwriteconfig6 --file kdeglobals --group Icons --key Theme "YAMIS"
-#killall plasmashell
-#plasmashell >/dev/null 2>&1 &
-#disown
-#ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/KDE-THEMES/Bibata-Modern-Ice/" "$HOME/.local/share/icons/Bibata-Modern-Ice/"
-#plasma-apply-cursortheme Bibata-Modern-Ice
-
-# SDDM theme
-
-#sudo pacman -Syu sddm
-#sudo systemctl enable sddm.service
-#sudo ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/KDE-THEMES/monochrome/" "/usr/share/sddm/themes/monochrome"
-#sudo ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/sddm.conf" "/etc/sddm.conf"
 
 echo Everything should be installed now! Please reboot your computer.
