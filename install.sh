47e0df1255ee0f916a0006169be6df557550f156
LunarGalaxy <johnnypetrie17@gmail.com>
2026-01-07 07:17:29 -0800

    Add lookandfeeltool command for KDE theme

--- install.sh
+++ install.sh
@@ -33,3 +33,5 @@ sudo pacman -Syu fastfetch
 
 mkdir -p "$HOME/.config/fastfetch"
 ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/FastFetchConfig.jsonc" "$HOME/.config/fastfetch/config.jsonc"
+
+lookandfeeltool -a org.kde.breezedark.desktop
