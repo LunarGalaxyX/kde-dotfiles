4a3075b775d52e4326583a329ce7c4d0a3ddd302
LunarGalaxy <johnnypetrie17@gmail.com>
2026-01-07 10:41:20 -0800

    Clean up install script and add decorative output
    Removed installation commands for various packages and configurations, while adding a decorative echo statement.

--- install.sh
+++ install.sh
@@ -1,6 +1,29 @@
 
 echo  
 echo  
+echo ⣷⢩⣞⣜⡯⣺⢵⠃⢐⡶⣯⡃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⢾⢃⡀⠈⢻⢯⢻⣜⠷⣢⢏⢶⡹⡼⢾
+⡗⢯⡾⣮⣳⢽⠃⣠⡟⣭⢷⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠉⢓⠄⠈⠶⣋⢶⡹⡜⣎⡯⡵⡫⣗
+⡽⢾⣻⢮⢷⡋⢀⣭⡻⡚⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣣⠀⠘⡱⢞⣜⣻⢒⣼⡺⣵⢞
+⣟⣱⢯⣟⣳⠀⣸⡞⡆⠹⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣓⠀⠙⣎⠷⢲⣍⠧⣗⠬⣏
+⣧⢾⣳⠿⡲⠀⣻⢺⣁⠀⠀⠀⠀⠀⠀⠀⠘⡄⠀⢰⠈⣷⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⣏⠇⠀⠯⣹⢎⡶⣫⢮⢯⢏
+⡞⣣⡽⢽⢣⡀⠷⢫⢝⠀⠂⠀⠀⠀⠀⠀⠀⠁⠀⠈⠀⠹⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⡝⠆⠀⣹⣣⢦⢫⢵⢻⡲⠽
+⣟⢳⡇⢯⡽⣁⠸⡏⣞⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠇⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⢎⡅⠀⡹⣌⠧⢛⢶⡱⢏⡳
+⢮⣉⠯⢳⣜⢧⡀⠻⡴⢋⡖⡆⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠈⠃⠀⠀⢀⡂⠀⢀⠄⠰⠈⠱⠎⠀⠀⢥⠮⠼⣹⢴⡙⡥⣓
+⡭⠞⣝⣹⠜⣭⠶⡄⠈⠃⠀⠀⠀⢰⣶⣦⣤⣤⣀⣀⣀⣀⣀⣤⣤⣴⣶⠾⣿⣿⠈⢢⠀⠀⠀⠀⠀⠀⠀⠩⢓⡱⢨⢆⡏⡜⣣
+⣎⢽⣢⢥⢫⡔⡟⣧⡓⠀⠀⠀⠀⠀⣿⣿⡿⠉⢻⣿⣿⣿⣿⣿⣿⣿⡇⠀⠘⣿⠐⢂⠁⠀⠀⠀⠀⠀⠀⠀⢣⡸⢅⡼⠐⣭⢰
+⠾⢶⠉⣎⢷⡸⣎⣱⠀⠰⠀⠀⠀⠀⢿⣿⣿⠀⠀⣿⣿⣿⣿⣿⣿⣿⣷⡀⢀⣿⠀⢁⠆⠀⠀⠀⠀⠀⠀⠀⢇⡸⢇⢶⢹⡰⡎
+⡙⣮⠕⡌⡆⢵⡐⣳⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣴⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⠐⠡⠂⠀⠀⠀⠀⠀⡀⣩⠒⡌⢰⢢⢡⠒⡥
+⡱⠔⣚⡜⠌⡣⠝⡤⢣⢠⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⠟⠻⣿⣿⣿⣿⣿⣿⣿⠈⡘⡁⠀⠀⠀⢠⡈⠴⡁⡳⢌⠰⣁⢎⡹⠖
+⡬⢱⡨⢔⠣⢵⣋⠵⣠⠇⠮⡄⠤⠴⠀⢿⣿⣿⣿⣿⣿⣦⠀⢹⣿⡇⢸⣿⣿⣿⠐⠤⠃⠀⠄⡚⠤⢉⠒⠦⢱⠈⢖⠥⠒⡡⡙
+⢂⠣⢔⠊⡑⠆⡔⡊⠔⣢⠱⠈⠑⠈⠁⣿⣿⣿⣿⣿⣿⣿⡆⢸⣿⠀⣾⣿⣿⣿⣇⠒⠉⢀⠀⠁⠂⠁⠚⠀⠃⠍⠰⠡⢃⡰⢁
+⡡⢊⠤⢉⠄⡹⣈⠴⠩⠠⠂⠈⢱⠀⠀⠈⠉⠛⠛⠻⠿⠿⠇⢨⣿⠀⠿⠟⠛⠉⠀⠀⠘⢈⠢⠁⠀⠄⡠⣤⣤⠀⠀⠀⠀⠐⢂
+⢂⠅⠊⠌⢒⡡⢀⣶⣶⣿⣿⣆⠈⠀⠀⠀⠉⠁⠈⢹⣷⣶⡆⢸⡇⠀⣤⠀⡄⠃⠀⠀⠀⠡⠌⠁⠁⡃⠄⣿⣿⠁⠀⠀⠀⠀⠀
+⠊⠀⠈⠀⢸⠀⠀⢹⣿⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀⠘⣿⣿⣷⠘⡇⢠⣿⡇⠘⠀⠀⠀⠀⠐⠡⠀⡑⢠⢢⣿⣿⠀⠰⠀⠀⠀⠀
+⠀⠀⠀⠀⠈⢰⠀⠀⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⣿⣿⣏⠀⠠⣸⣿⡇⠀⠃⠀⠀⠀⠀⠑⠀⡐⢃⢸⣿⣿⠀⠃⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⡗⠀⠀⠀⠀⡂⢹⣿⣇⡆⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠐⡈⠆⣹⡟⢿⠀⠀⠀⡀⡀⠀
+⠀⠀⠀⠀⠀⠠⠀⠀⠈⣿⣿⣿⣿⠏⠀⡇⠀⠀⠀⠀⠸⣿⣿⣹⣿⣿⣿⣿⠀⡀⠀⠀⠀⠀⢂⠀⠀⠁⠘⠻⢇⠀⠀⠀⠃⠀⠀
+⣤⠖⠀⠀⠀⠀⠀⠀⠀⢻⣿⡟⠁⠠⠀⠙⠓⠀⠀⠀⠀⣿⣿⣿⣿⠛⠉⠉⠀⠀⢠⠜⡀⠀⠂⠌⠐⠄⢀⠀⡀⠀⠀⠀⠀⠀⠀
+⠁⠀⠀⠀⠀⠀⠀⠀⠀⠰⠋⠠⢌⠁⢊⠀⠀⢰⣴⣀⣀⣉⡩⠉⢉⣤⣤⣀⣜⣀⣧⢂⠅⠀⠐⡈⠰⠈⠄⠠⠀⠃⠐⠀⡀⠀⠄
 echo Hi, thank you for trying out my Punpun dotfiles!
 echo  
 echo  
@@ -9,42 +32,50 @@ sleep 2
 
 sudo pacman -Syu plasma
 
-sudo pacman -Syu kitty
+# Kitty config
 
-mkdir -p "$HOME/.config/kitty"
-ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/KittyConfig.conf" "$HOME/.config/kitty/kitty.conf"
+#sudo pacman -Syu kitty
+#mkdir -p "$HOME/.config/kitty"
+#ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/KittyConfig.conf" "$HOME/.config/kitty/kitty.conf"
 
-sudo pacman -Syu zsh
+# Zsh configs and theme
 
-sudo pacman -Syu curl
+#sudo pacman -Syu zsh
+#sudo pacman -Syu curl
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#sudo pacman -Syu ttf-meslo-nerd-font-powerlevel10k
+#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+#mkdir -p "$HOME/.config/zsh"
+#ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/ZshellConfig" "$HOME/.config/zsh/.zshrc"
+#ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/P10kThemeConfig.zsh" "$HOME/.config/zsh/.p10k.zsh"
 
-sudo pacman -Syu git
+# Fastfetch and configs
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#sudo pacman -Syu fastfetch
+#mkdir -p "$HOME/.config/fastfetch"
+#ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/FastFetchConfig.jsonc" "$HOME/.config/fastfetch/config.jsonc"
 
-sudo pacman -Syu ttf-meslo-nerd-font-powerlevel10k
+# Plasma configs and themes.
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+#plasma-apply-wallpaperimage "$HOME/punpun-rice-dotfiles/dotfiles/Wallpapers/punpunWallpaper.png"
+#lookandfeeltool -a org.kde.breezedark.desktop
+#ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/KDE-THEMES/MateriaDark.colors" "$HOME/.local/share/color-schemes/MateriaDark.colors"
+#plasma-apply-colorscheme MateriaDark
+#ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/KDE-THEMES/Materia/" "$HOME/.local/share/plasma/desktoptheme/Materia"
+#plasma-apply-desktoptheme Materia
+#ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/KDE-THEMES/YAMIS/" "$HOME/.local/share/icons/YAMIS/"
+#kwriteconfig6 --file kdeglobals --group Icons --key Theme "YAMIS"
+#killall plasmashell
+#plasmashell >/dev/null 2>&1 &
+#disown
+#ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/KDE-THEMES/Bibata-Modern-Ice/" "$HOME/.local/share/icons/Bibata-Modern-Ice/"
+#plasma-apply-cursortheme Bibata-Modern-Ice
 
-mkdir -p "$HOME/.config/zsh"
-ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/ZshellConfig" "$HOME/.config/zsh/.zshrc"
+# SDDM theme
 
-ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/P10kThemeConfig.zsh" "$HOME/.config/zsh/.p10k.zsh"
+#sudo pacman -Syu sddm
+#sudo systemctl enable sddm.service
+#sudo ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/KDE-THEMES/monochrome/" "/usr/share/sddm/themes/monochrome"
+#sudo ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/sddm.conf" "/etc/sddm.conf"
 
-sudo pacman -Syu fastfetch
-
-mkdir -p "$HOME/.config/fastfetch"
-ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/FastFetchConfig.jsonc" "$HOME/.config/fastfetch/config.jsonc"
-
-plasma-apply-wallpaperimage "$HOME/punpun-rice-dotfiles/dotfiles/Wallpapers/punpunWallpaper.png"
-
-lookandfeeltool -a org.kde.breezedark.desktop
-
-ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/KDE-THEMES/MateriaDark.colors" "$HOME/.local/share/color-schemes/MateriaDark.colors"
-plasma-apply-colorscheme MateriaDark
-
-ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/KDE-THEMES/Materia/" "$HOME/.local/share/plasma/desktoptheme/Materia"
-plasma-apply-desktoptheme Materia
-
-ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/KDE-THEMES/Nothing/" "$HOME/.local/share/aurorae/themes/Nothing"
-ln -sfb "$HOME/punpun-rice-dotfiles/dotfiles/kwinrc" "$HOME/.config/kwinrc"
+echo Everything should be installed now! Please reboot your computer.
